5a37812b9b3303a31fd714d4420d18a08c2767ad
Austen Sulikowski <Austenski@gmail.com>
2021-04-27 16:25:46 -0400

    added css and ascending buttons as well as search bar

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,15 +17,61 @@ function Table() {
     console.log(vehicleInfo.make);
     console.log(vehicleInfo.model);
   }
+  function sortBy(key) {
+    this.setState({
+      vehicleInfo: vehicleInfo.sort((a, b) => a[key] < b[key]),
+    });
+  }
   return (
-    <div id="TablePage">
-      {vehicleInfo !== "" ? (
-        <div id="vehicleList">
-          {vehicleInfo.year.type} {vehicleInfo.make.type} {vehicleInfo.model.type}
-        </div>
-      ) : (
-        "Loading..."
-      )}
+    <div id="searchBar">
+      <input type="search" placeholder="Search..."></input>
+
+      <div id="TablePage">
+        <table>
+          <tr id="Columns">
+            <th>
+              <button onCLick={() => sortBy("year")}> Vehicle Year </button>
+            </th>
+
+            <th>
+              <button onCLick={() => sortBy("make")}> Vehicle Make </button>
+            </th>
+
+            <th>
+              <button onCLick={() => sortBy("model")}> Vehicle Model </button>
+            </th>
+          </tr>
+
+          {vehicleInfo !== "" ? (
+            <div>
+              <tbody>
+                <tr id="TableContents">
+                  <td>{vehicleInfo.year.type}</td>
+                  <td>{vehicleInfo.make.type}</td>
+                  <td>{vehicleInfo.model.type}</td>
+                </tr>
+                <tr id="addedAudi">
+                  <td>2020 </td>
+                  <td>Audi</td>
+                  <td>A3</td>
+                </tr>
+                <tr id="addedBMW">
+                  <td>2013</td>
+                  <td>BMW</td>
+                  <td>X3</td>
+                </tr>
+                <tr id="addedFord">
+                  <td>1988</td>
+                  <td>Ford</td>
+                  <td>EXP</td>
+                </tr>
+              </tbody>
+            </div>
+          ) : (
+            "Loading..."
+          )}
+        </table>
+      </div>
     </div>
   );
 }
